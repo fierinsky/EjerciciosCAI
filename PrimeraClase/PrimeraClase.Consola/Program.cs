977f544433b5c0f3c4f53c90655a94a04f674a18
fierinsky <38143280+fierinsky@users.noreply.github.com>
2021-08-24 22:58:42 -0300

    proyecto

---
+++ PrimeraClase/PrimeraClase.Consola/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using PrimeraClase.Validacion;
+
+
+namespace PrimeraClase.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo.");
+
+        }
+    }
+}
