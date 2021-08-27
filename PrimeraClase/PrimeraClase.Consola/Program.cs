11a9eb2b258368b9ff721973ed5325258351ba17
fierinsky <38143280+fierinsky@users.noreply.github.com>
2021-08-27 16:23:31 -0300

    calculadora

--- PrimeraClase/PrimeraClase.Consola/Program.cs
+++ PrimeraClase/PrimeraClase.Consola/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
 using PrimeraClase.Validacion;
 
-
 namespace PrimeraClase.Consola
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Mundo.");
-
+            string input;
+            byte codigo = 0;
+            float resultado = 0;
+            float operando1 = 0;
+            float operando2 = 0;
+            do
+            {
+                Console.Write("1 Suma +" + Environment.NewLine + "2 Resta -" + Environment.NewLine + "3 División /"
+                    + Environment.NewLine + "4 Multiplicación *" + Environment.NewLine + "5 Potenciación ^" +
+                    Environment.NewLine + "9 Salir" + Environment.NewLine +
+                    "Ingrese el número de la operación que desea realiar: ");
+                input = Console.ReadLine();
+                if (!Byte.TryParse(input, out codigo))
+                    Console.WriteLine("Código erróneo");
+                else if (codigo == 1 || codigo == 2 || codigo == 3 || codigo == 4 || codigo == 5)
+                {
+                    operando1 = ingresarNum();
+                    operando2 = ingresarNum();
+                    resultado = CalcularOperacion(codigo, operando1, operando2);
+                    MostrarResultado(codigo, operando1, operando2, resultado);
+                }
+            }
+            while (codigo != 9);
         }
     }
 }
--- PrimeraClase/PrimeraClase.Validacion/Validador.cs
+++ PrimeraClase/PrimeraClase.Validacion/Validador.cs
@@ -1,13 +1,78 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PrimeraClase.Validacion
 {
     public class Validador
     {
-        
+        private static float ingresarNum()
+        {
+            bool flag;
+            string operando;
+            float op;
+            do
+            {
+                Console.Write("Ingrese un número: ");
+                operando = Console.ReadLine();
+                if (!float.TryParse(operando, out op))
+                {
+                    Console.WriteLine("Ingrese un valor numérico");
+                    flag = true;
+                }
+                else flag = false;
+            }
+            while (flag);
+            return op;
+        }
+
+        private static float CalcularOperacion(byte cod, float op1, float op2)
+        {
+            float res = 0;
+            switch (cod)
+            {
+                case 1:
+                    res = op1 + op2;
+                    break;
+                case 2:
+                    res = op1 - op2;
+                    break;
+                case 3:
+                    if (op2 != 0)
+                        res = op1 / op2;
+                    break;
+                case 4:
+                    res = op1 * op2;
+                    break;
+
+                case 5:
+                    res = Convert.ToSingle(Math.Pow(op1, op2));
+                    break;
+            }
+            return res;
+        }
+
+        private static void MostrarResultado(byte cod, float op1, float op2, float res)
+        {
+            switch (cod)
+            {
+                case 1:
+                    Console.WriteLine(op1 + " + " + op2 + " = " + res); ;
+                    break;
+                case 2:
+                    Console.WriteLine(op1 + " - " + op2 + " = " + res); ;
+                    break;
+                case 3:
+                    if (op2 == 0)
+                        Console.WriteLine("Error, división por 0");
+                    else
+                        Console.WriteLine(op1 + " / " + op2 + " = " + res); ;
+                    break;
+                case 4:
+                    Console.WriteLine(op1 + " * " + op2 + " = " + res); ;
+                    break;
+                case 5:
+                    Console.WriteLine(op1 + " ^ " + op2 + " = " + res);
+                    break;
+            }
+        }
     }
 }
