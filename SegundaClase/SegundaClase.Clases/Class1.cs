917f9905df48a76ce4193e6c95bb0094c01e0539
fierinsky <38143280+fierinsky@users.noreply.github.com>
2021-08-27 22:53:05 -0300

    segunda clase

---
+++ SegundaClase/SegundaClase.Clases/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegundaClase.Clases
+{
+    public class ClienteBancario
+    {
+        private int _numeroDocumento;
+        private string _nombre;
+        private string _apellido;
+        private DateTime _edad;
+
+        //propiedades, getter y setter
+        public int NumeroDocumento
+        {
+            get
+            {
+                return _numeroDocumento;
+            }
+            set
+            {
+                _numeroDocumento = value;
+            }
+        }
+
+    }
+}
---
+++ SegundaClase/SegundaClase.Consola/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SegundaClase.Clases;
+
+namespace SegundaClase.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClienteBancario cliente = new ClienteBancario();
+            cliente.NumeroDocumento
+        }
+    }
+}
