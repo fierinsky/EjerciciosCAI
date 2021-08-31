bf7194e19a63b02afa3bd9a1500abc4d7e241ca2
fierinsky <38143280+fierinsky@users.noreply.github.com>
2021-08-31 15:40:06 -0300

    almacen poo

---
+++ Almacen/Almacen.Clases/Class1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+
+namespace Almacen.Clases
+{
+    public class Almacen
+    {
+        private string _nombre;
+        private string _direccion;
+        private string _razonSocial;
+        private Producto[] _productos;
+        private Empleado[] _empleados;
+
+        public string Nombre
+        {
+            get
+            {
+                return _nombre;
+            }
+            set
+            {
+                _nombre = value;
+            }
+        }
+
+        public string Direccion
+        {
+            get
+            {
+                return _direccion;
+            }
+            set
+            {
+                _direccion = value;
+            }
+        }
+
+        public string RazonSocial
+        {
+            get
+            {
+                return _razonSocial;
+            }
+            set
+            {
+                _razonSocial = value;
+            }
+        }
+
+        public Empleado[] Empleados
+        {
+            get
+            {
+                return _empleados;
+            }
+            set
+            {
+                _empleados = value;
+            }
+        }
+
+        public Producto[] Productos
+        {
+            get
+            {
+                return _productos;
+            }
+            set
+            {
+                _productos = value;
+            }
+        }
+    }
+
+    public class Producto
+    {
+        private string _nombre;
+        private int _id;
+        private float _precio;
+        private int _stock;
+
+        public string Nombre
+        {
+            get
+            {
+                return _nombre;
+            }
+            set
+            {
+                _nombre = value;
+            }
+        }
+
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+            }
+        }
+
+        public float Precio
+        {
+            get
+            {
+                return _precio;
+            }
+            set
+            {
+                _precio = value;
+            }
+        }
+
+        public int Stock
+        {
+            get
+            {
+                return _stock;
+            }
+            set
+            {
+                _stock = value;
+            }
+        }
+    }
+
+    public class Empleado
+    {
+        private string _nombre;
+        private int _dni;
+        private float _sueldo;
+        private int _edad;
+        private int _antiguedad;
+        private string _puesto;
+
+        public string Nombre
+        {
+            get
+            {
+                return _nombre;
+            }
+            set
+            {
+                _nombre = value;
+            }
+        }
+
+        public int DNI
+        {
+            get
+            {
+                return _dni;
+            }
+            set
+            {
+                _dni = value;
+            }
+        }
+
+        public float Sueldo
+        {
+            get
+            {
+                return _sueldo;
+            }
+            set
+            {
+                _sueldo = value;
+            }
+        }
+
+        public int Edad
+        {
+            get
+            {
+                return _edad;
+            }
+            set
+            {
+                _edad = value;
+            }
+        }
+
+        public int Antiguedad
+        {
+            get
+            {
+                return _antiguedad;
+            }
+            set
+            {
+                _antiguedad = value;
+            }
+        }
+
+        public string Puesto
+        {
+            get
+            {
+                return _puesto;
+            }
+            set
+            {
+                _puesto = value;
+            }
+        }
+    }
+}
---
+++ Almacen/Almacen/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Almacen.Clases;
+namespace Almacen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Almacen.Clases.Almacen almacen = new Clases.Almacen();
+            Producto papa = new Producto();
+            Producto detergente = new Producto();
+            Empleado cajero = new Empleado();
+            Empleado limpieza = new Empleado();
+            Empleado encargado = new Empleado();
+
+            almacen.Nombre = "El Chino de la Esquina";
+            almacen.Direccion = "Cochabamba 3470";
+            almacen.RazonSocial = "Chino S.A.";
+
+            papa.Id = 1;
+            papa.Nombre = "Papa";
+            papa.Precio = 15.5f;
+            papa.Stock = 40;
+
+            detergente.Id = 2;
+            detergente.Nombre = "Mr. Músculo";
+            detergente.Precio = 84.95f;
+            detergente.Stock = 25;
+
+            cajero.Nombre = "Carlos García";
+            cajero.DNI = 44531960;
+            cajero.Edad = 18;
+            cajero.Antiguedad = 6;
+            cajero.Puesto = "Cajero";
+            cajero.Sueldo = 20000f;
+
+            limpieza.Nombre = "Pedro Perez";
+            limpieza.DNI = 37556960;
+            limpieza.Edad = 25;
+            limpieza.Antiguedad = 18;
+            limpieza.Puesto = "Limpieza";
+            limpieza.Sueldo = 30000f;
+
+            encargado.Nombre = "Lorena Lopez";
+            encargado.DNI = 34093812;
+            encargado.Edad = 28;
+            encargado.Antiguedad = 32;
+            encargado.Puesto = "Encargada";
+            encargado.Sueldo = 60000f;
+
+            almacen.Productos[1] = detergente;
+            almacen.Productos[0] = papa;
+
+            almacen.Empleados[0] = limpieza;
+            almacen.Empleados[1] = encargado;
+            almacen.Empleados[2] = cajero;
+
+        }
+    }
+}
