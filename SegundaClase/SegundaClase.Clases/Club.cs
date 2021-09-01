a47b66b0fec4c6d7c8df9d841d7962521cb8f709
fierinsky <38143280+fierinsky@users.noreply.github.com>
2021-08-31 21:50:48 -0300

    reordenacion clases futbolista

---
+++ SegundaClase/SegundaClase.Clases/Club.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegundaClase.Clases
+{
+    public class Club
+    {
+        private int _idClub;
+        private string _nombre;
+        private DateTime _fundacion;
+        private Futbolista[] _futbolistas;
+
+        public int IdClub
+        {
+            get
+            {
+                return _idClub;
+            }
+            set
+            {
+                _idClub = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return _nombre;
+            }
+            set
+            {
+                _nombre = value;
+            }
+        }
+
+        public DateTime Fundacion
+        {
+            get
+            {
+                return _fundacion;
+            }
+            set
+            {
+                _fundacion = value;
+            }
+        }
+
+        public Futbolista[] Futbolistas
+        {
+            get
+            {
+                return _futbolistas;
+            }
+            set
+            {
+                _futbolistas = value;
+            }
+        }
+    }
+}
